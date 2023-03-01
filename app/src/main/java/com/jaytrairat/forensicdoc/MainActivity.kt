ec1c94c253dbbb0e182f388ba551190483aece8d
jaytrairat <jaytrairat@outlook.com>
2023-03-01 16:13:48 +0700

    feat: create document

--- app/src/main/java/com/jaytrairat/forensicdoc/MainActivity.kt
+++ app/src/main/java/com/jaytrairat/forensicdoc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jaytrairat.forensicdoc
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +13,10 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import org.apache.poi.xwpf.usermodel.XWPFDocument
 import org.apache.xmlbeans.XmlOptions
+import java.io.File
+import java.io.FileOutputStream
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 
@@ -71,44 +76,44 @@ class MainActivity : AppCompatActivity() {
             val templateInputStream = resources.openRawResource(R.raw.indexpage)
             val document = XWPFDocument(templateInputStream)
 
-//            // Replace placeholders with data from the TextView
-//            val data = mapOf(
-//                "document_to" to documentTo,
-//                "original_form" to originalFrom,
-//                "original_number" to originalNumber,
-//                "original_date" to originalDate,
-//                "original_name" to originalName,
-//                "number_of_result" to numberOfPages,
-//            )
-//            for (paragraph in document.paragraphs) {
-//                for (run in paragraph.runs) {
-//                    var text = run.text()
-//                    for ((key, value) in data) {
-//                        text = text.replace("{$key}", value)
-//                    }
-//                    run.setText(text, 0)
-//                }
-//            }
-//
-//            // Save the filled template as a new docx file in the Downloads folder
-//            val now = LocalDateTime.now()
-//            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
-//            var index = 0
-//            var outputFile: File
-//            do {
-//                val filename = "${formatter.format(now)}_${index}_page.docx"
-//                outputFile = File(
-//                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-//                    filename
-//                )
-//                index++
-//            } while (outputFile.exists())
-//
-//            val fos = FileOutputStream(outputFile)
-//            document.write(fos)
-//            fos.close()
-//
-//            // Close the template document
+            // Replace placeholders with data from the TextView
+            val data = mapOf(
+                "document_to" to documentTo,
+                "original_form" to originalFrom,
+                "original_number" to originalNumber,
+                "original_date" to originalDate,
+                "original_name" to originalName,
+                "number_of_result" to numberOfPages,
+            )
+            for (paragraph in document.paragraphs) {
+                for (run in paragraph.runs) {
+                    var text = run.text()
+                    for ((key, value) in data) {
+                        text = text.replace("{$key}", value)
+                    }
+                    run.setText(text, 0)
+                }
+            }
+
+            // Save the filled template as a new docx file in the Downloads folder
+            val now = LocalDateTime.now()
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
+            var index = 0
+            var outputFile: File
+            do {
+                val filename = "${formatter.format(now)}_${index}_page.docx"
+                outputFile = File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                    filename
+                )
+                index++
+            } while (outputFile.exists())
+
+            val fos = FileOutputStream(outputFile)
+            document.write(fos)
+            fos.close()
+
+            // Close the template document
             document.close()
             Toast.makeText(this, "Document created", Toast.LENGTH_LONG).show()
         } catch (error: Exception) {
